ca5b812763e4036ab3cf5173cef5303ff753cc41
veysiertekin <ertekin.veysi@gmail.com>
2015-01-08 01:15:25 +0200

    init

---
+++ js/background.js
@@ -0,0 +1,193 @@
+/**
+  head.load.min.js ++++++++++++++++++++++++++++++++
+*/
+(function(f,w){function m(){}function g(a,b){if(a){"object"===typeof a&&(a=[].slice.call(a));for(var c=0,d=a.length;c<d;c++)b.call(a,a[c],c)}}function v(a,b){var c=Object.prototype.toString.call(b).slice(8,-1);return b!==w&&null!==b&&c===a}function k(a){return v("Function",a)}function h(a){a=a||m;a._done||(a(),a._done=1)}function n(a){var b={};if("object"===typeof a)for(var c in a)a[c]&&(b={name:c,url:a[c]});else b=a.split("/"),b=b[b.length-1],c=b.indexOf("?"),b={name:-1!==c?b.substring(0,c):b,url:a};return(a=p[b.name])&&a.url===b.url?a:p[b.name]=b}function q(a){var a=a||p,b;for(b in a)if(a.hasOwnProperty(b)&&a[b].state!==r)return!1;return!0}function s(a,b){b=b||m;a.state===r?b():a.state===x?d.ready(a.name,b):a.state===y?a.onpreload.push(function(){s(a,b)}):(a.state=x,z(a,function(){a.state=r;b();g(l[a.name],function(a){h(a)});j&&q()&&g(l.ALL,function(a){h(a)})}))}function z(a,b){var b=b||m,c;/\.css[^\.]*$/.test(a.url)?(c=e.createElement("link"),c.type="text/"+(a.type||"css"),c.rel="stylesheet",c.href=a.url):(c=e.createElement("script"),c.type="text/"+(a.type||"javascript"),c.src=a.url);c.onload=c.onreadystatechange=function(a){a=a||f.event;if("load"===a.type||/loaded|complete/.test(c.readyState)&&(!e.documentMode||9>e.documentMode))c.onload=c.onreadystatechange=c.onerror=null,b()};c.onerror=function(){c.onload=c.onreadystatechange=c.onerror=null;b()};c.async=!1;c.defer=!1;var d=e.head||e.getElementsByTagName("head")[0];d.insertBefore(c,d.lastChild)}function i(){e.body?j||(j=!0,g(A,function(a){h(a)})):(f.clearTimeout(d.readyTimeout),d.readyTimeout=f.setTimeout(i,50))}function t(){e.addEventListener?(e.removeEventListener("DOMContentLoaded",t,!1),i()):"complete"===e.readyState&&(e.detachEvent("onreadystatechange",t),i())}var e=f.document,A=[],B=[],l={},p={},E="async"in e.createElement("script")||"MozAppearance"in e.documentElement.style||f.opera,C,j,D=f.head_conf&&f.head_conf.head||"head",d=f[D]=f[D]||function(){d.ready.apply(null,arguments)},y=1,x=3,r=4;d.load=E?function(){var a=arguments,b=a[a.length-1],c={};k(b)||(b=null);g(a,function(d,e){d!==b&&(d=n(d),c[d.name]=d,s(d,b&&e===a.length-2?function(){q(c)&&h(b)}:null))});return d}:function(){var a=arguments,b=[].slice.call(a,1),c=b[0];if(!C)return B.push(function(){d.load.apply(null,a)}),d;c?(g(b,function(a){if(!k(a)){var b=n(a);b.state===w&&(b.state=y,b.onpreload=[],z({url:b.url,type:"cache"},function(){b.state=2;g(b.onpreload,function(a){a.call()})}))}}),s(n(a[0]),k(c)?c:function(){d.load.apply(null,b)})):s(n(a[0]));return d};d.js=d.load;d.test=function(a,b,c,e){a="object"===typeof a?a:{test:a,success:b?v("Array",b)?b:[b]:!1,failure:c?v("Array",c)?c:[c]:!1,callback:e||m};(b=!!a.test)&&a.success?(a.success.push(a.callback),d.load.apply(null,a.success)):!b&&a.failure?(a.failure.push(a.callback),d.load.apply(null,a.failure)):e();return d};d.ready=function(a,b){if(a===e)return j?h(b):A.push(b),d;k(a)&&(b=a,a="ALL");if("string"!==typeof a||!k(b))return d;var c=p[a];if(c&&c.state===r||"ALL"===a&&q()&&j)return h(b),d;(c=l[a])?c.push(b):l[a]=[b];return d};d.ready(e,function(){q()&&g(l.ALL,function(a){h(a)});d.feature&&d.feature("domloaded",!0)});if("complete"===e.readyState)i();else if(e.addEventListener)e.addEventListener("DOMContentLoaded",t,!1),f.addEventListener("load",i,!1);else{e.attachEvent("onreadystatechange",t);f.attachEvent("onload",i);var u=!1;try{u=null==f.frameElement&&e.documentElement}catch(F){}u&&u.doScroll&&function b(){if(!j){try{u.doScroll("left")}catch(c){f.clearTimeout(d.readyTimeout);d.readyTimeout=f.setTimeout(b,50);return}i()}}()}setTimeout(function(){C=!0;g(B,function(b){b()})},300)})(window);
+/* ---------------------------------------------- */
+var img = new Image();
+
+function drawIcon() {
+    if (localStorage['ready'] && localStorage['ready']=="true") {
+        var currentDate_ = new Date();
+        var nextDate_ = new Date()
+        nextDate_.setDate(nextDate_.getDate()+1);
+
+        var currentTime = currentDate_.format('hh:mm');
+
+        var day = getTimesOfDay(currentDate_);
+        var nextDay = getTimesOfDay(nextDate_);
+
+        var prevTimeZone = localStorage['currentTimeZone'];
+        var prevFollowTimeZone = localStorage['nextTimeZone'];
+
+        var diff = null;
+        for(i=0;i<1;i++){
+            diff = timeDiff(currentTime, day.fajr);
+            if (diff.hours>=0 && diff.minutes>0) {
+                localStorage['currentTimeZone']="isha";
+                localStorage['nextTimeZone']="fajr";
+                break;
+            }
+            diff = timeDiff(currentTime, day.sunrise);
+            if (diff.hours>=0 && diff.minutes>0) {
+                localStorage['currentTimeZone']="fajr";
+                localStorage['nextTimeZone']="sunrise";
+               break;
+            }
+            diff = timeDiff(currentTime, day.zuhr);
+            if (diff.hours>=0 && diff.minutes>0) {
+                localStorage['currentTimeZone']="sunrise";
+                localStorage['nextTimeZone']="zuhr";
+               break;
+            }
+            diff = timeDiff(currentTime, day.asr);
+            if (diff.hours>=0 && diff.minutes>0) {
+                localStorage['currentTimeZone']="zuhr";
+                localStorage['nextTimeZone']="asr";
+               break;
+            }
+            diff = timeDiff(currentTime, day.maghrib);
+            if (diff.hours>=0 && diff.minutes>0) {
+                localStorage['currentTimeZone']="asr";
+                localStorage['nextTimeZone']="maghrib";
+               break;
+            }
+            diff = timeDiff(currentTime, day.isha);
+            if (diff.hours>=0 && diff.minutes>0) {
+                localStorage['currentTimeZone']="maghrib";
+                localStorage['nextTimeZone']="isha";
+               break;
+            }
+            if (nextDay!=null) {
+                var nextFajr = nextDay.fajr.split(":");
+                diff = timeDiff(currentTime, (((parseInt(nextFajr[0])+24)+"").pad(2, "0")+":"+nextFajr[1]));
+            } else {
+                diff = timeDiff(currentTime, "23:59");
+            }
+            localStorage['currentTimeZone']="isha";
+            localStorage['nextTimeZone']="fajr";
+        }
+
+        var nextMinute = parseInt(localStorage["notify_"+localStorage['nextTimeZone']+"_minute"]);
+        var minute = parseInt(localStorage["notify_"+localStorage['currentTimeZone']+"_minute"]);
+
+        if (localStorage["notify_"+localStorage['currentTimeZone']+"_state"] && localStorage["notify_"+localStorage['currentTimeZone']+"_state"]=="on") {
+            if (prevTimeZone && prevTimeZone!=localStorage['currentTimeZone']) {
+                localStorage["notify_"+prevTimeZone+"_ok"]="";
+                if (minute==0){ // Time changed
+                    show("", "");
+                    localStorage["notify_"+localStorage['currentTimeZone']+"_ok"]="ok";
+                }
+            }
+        }
+
+        if (localStorage["notify_"+localStorage['nextTimeZone']+"_state"]=="on" &&
+            (!localStorage["notify_"+localStorage['nextTimeZone']+"_ok"] || localStorage["notify_"+localStorage['nextTimeZone']+"_ok"]=="") &&
+            (diff.hours*60+diff.minutes)<=nextMinute){
+            show("", "");
+            localStorage["notify_"+localStorage['nextTimeZone']+"_ok"]="ok";
+        }
+
+        var element = document.createElement("canvas");
+
+        element.width = 19;
+        element.height = 19;
+        var ctx = element.getContext("2d");
+
+        ctx.drawImage(img, 0, 0);
+        if (!localStorage['time_watcher']){
+            localStorage["time_watcher"]="30";
+        }
+        if (diff.hours==0 && diff.minutes<=parseInt(localStorage["time_watcher"])) {
+            chrome.browserAction.setBadgeBackgroundColor({ color: [225, 0, 0, 255] });
+        } else {
+            chrome.browserAction.setBadgeBackgroundColor({ color: [0, 100, 175, 255] });
+        }
+
+        var badgeText="";
+        if (diff.hours>9) {
+            ctx.beginPath();
+            ctx.moveTo(0, 17);
+            ctx.lineTo(0, 12);
+            ctx.quadraticCurveTo(0, 10, 2, 10);
+            ctx.lineTo(17, 10);
+            ctx.quadraticCurveTo(19, 10, 19, 12);
+            ctx.lineTo(19, 17);
+            ctx.quadraticCurveTo(19, 19, 17, 19);
+            ctx.lineTo(2, 19);
+            ctx.quadraticCurveTo(0, 19, 0, 17);
+            ctx.fillStyle = "yellow"; //"#f280a7";
+            ctx.fill();
+            ctx.strokeStyle = "#f0f0f0";
+            ctx.stroke();
+
+            ctx.lineWidth = 0.5;
+            ctx.lineWidth = 0.5;
+            ctx.fillStyle = "#000";
+            ctx.font = "8px Helvetica";
+
+            ctx.renderText((diff.hours>9?"":" ")+diff.hours+":"+(diff.minutes+"").pad(2, "0"), 0, 18, 0);            
+        } else {
+            badgeText = diff.hours+":"+(diff.minutes+"").pad(2, "0");
+        }
+
+        chrome.browserAction.setIcon({imageData:ctx.getImageData(0, 0, 19,19)});
+        chrome.browserAction.setBadgeText({ text: badgeText });
+    } else {
+        chrome.browserAction.setBadgeText({
+            text: "..."
+        });
+    }
+
+    setTimeout(function () { drawIcon() }, 500);
+}
+
+function init() {
+
+    setTimeout(function () {
+        init()
+    }, 1000);
+}
+
+function show(title, text) {
+    var notification = window.webkitNotifications.createNotification('', chrome.i18n.getMessage("title") + (title ? (" - " + title) : ""), text);
+    notification.onclick = function () {
+        this.cancel();
+    };
+
+    notification.show();
+    setTimeout(function () {
+        notification.cancel();
+    }, 15000);
+}
+
+head.js("js/jquery-1.10.2.js",
+    function () {
+        head.js("js/utils.js",
+            function () {
+                if (!localStorage['isNotFirstUsage'] && localStorage['isNotFirstUsage']!="true") {
+                  openOptionsTab(false);
+                }
+
+                localStorage['isNotFirstUsage']="true";
+
+                if (localStorage['version']!=chrome.app.getDetails().version && chrome.i18n.getMessage("version_update_text")!=""){
+                    updateTimes(localStorage["city"], function(times, isError){
+                        if (!isError){
+                            localStorage["times"]=JSON.stringify(times);
+                        }
+                    });
+                }
+
+                /* Start to drawing icon +++ */
+                addRenderTextMethodToCanvas();
+                img.src = 'img/icon_19.png';
+                img.onload = function () {
+                    drawIcon();
+                };
+                /* --- Start to drawing icon*/
+
+                init();
+            }
+        );
+    }
+);
---
+++ js/utils.js
@@ -0,0 +1,589 @@
+function addRenderTextMethodToCanvas() {
+	if (CanvasRenderingContext2D && !CanvasRenderingContext2D.renderText) {
+	    // @param  letterSpacing  {float}  CSS letter-spacing property
+	    CanvasRenderingContext2D.prototype.renderText = function (text, x, y, letterSpacing) {
+	        if (!text || typeof text !== 'string' || text.length === 0) {
+	            return;
+	        }
+
+	        if (typeof letterSpacing === 'undefined') {
+	            letterSpacing = 0;
+	        }
+
+	        // letterSpacing of 0 means normal letter-spacing
+
+	        var characters = String.prototype.split.call(text, ''),
+	            index = 0,
+	            current,
+	            currentPosition = x,
+	            align = 1;
+
+	        if (this.textAlign === 'right') {
+	            characters = characters.reverse();
+	            align = -1;
+	        } else if (this.textAlign === 'center') {
+	            var totalWidth = 0;
+	            for (var i = 0; i < characters.length; i++) {
+	                totalWidth += (this.measureText(characters[i]).width + letterSpacing);
+	            }
+	            currentPosition = x - (totalWidth / 2);
+	        }
+
+	        while (index < text.length) {
+	            current = characters[index++];
+	            this.fillText(current, currentPosition, y);
+	            currentPosition += (align * (this.measureText(current).width + letterSpacing));
+	        }
+	    }
+	};
+}
+
+function prepare(){
+	$('[data-resource]').each(function() {
+	  var el = $(this);
+	  var resourceName = el.data('resource');
+	  var resourceText = chrome.i18n.getMessage(resourceName);
+	  if (el.is("input")) {
+	  	el.val(resourceText);
+	  } else {
+	  	el.text(resourceText);
+	  }
+	});
+	$(".pieContainer").mousemove(function(e){
+      if ($(this).hasClass("disabled_pie")) {
+        return;
+      } 
+      var parentOffset = $(this).offset();
+
+      var x0= 0;
+      var y0= 0;
+        var x1=0;
+        var y1=1;
+        if (e.pageX){
+             x1 = e.pageX - parentOffset.left - $(this).outerWidth()/2;
+        }
+        if (e.pageY){
+             y1 = (e.pageY - parentOffset.top - $(this).outerHeight()/2)*(-1);
+        }
+
+        changePieSize(this, calcDegree(x0, y0,x1, y1));
+   });
+   $(".pieContainer").on("mouseleave", function(e){
+        $(".vacC", this).text($(".vacC", this).attr("data"));
+        changePieSize(this, parseInt($(".vacC", this).attr("data"))*6);
+   });
+   $(".pieContainer").click(function(e){
+        $(".vacC", this).attr({"data":$(".vacC", this).text()});
+   });
+   $(".pieContainer").trigger("click").trigger("mouseleave");
+	//updateCountries( function(countries, isError){});
+	//updateProvinces(1, function(provinces, isError){});
+	//updateCities(1, 133, function(cities, isError){});
+	//getTimes(133, function(times, isError){});
+}
+
+function openOptionsTab(isFromPopup){
+	chrome.tabs.query({}, function (tabs) {
+		var optionsPageUrl = getOptionsPageUrl();
+	    for (var i = 0; i < tabs.length; i++) {
+			if (tabs[i].url==optionsPageUrl) {
+				chrome.windows.update(tabs[i].windowId, {focused: true});
+				chrome.tabs.update(tabs[i].id, {selected: true});
+				chrome.tabs.reload(tabs[i].id, {});
+				if (isFromPopup) {
+					window.close();
+				}
+				return;
+			}
+		}
+		chrome.tabs.create({ url: optionsPageUrl });
+		if (isFromPopup) {
+			window.close();
+		}
+	});
+}
+
+function getOptionsPageUrl(){
+	return "chrome-extension://"+chrome.i18n.getMessage("@@extension_id")+"/options.html";
+}
+
+function invokeGetRequest(url, callback){
+	var isError=false;
+	var response=null;
+
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open('GET', url, true);
+	xmlhttp.onreadystatechange = function () {
+		isError=true;
+	    if (xmlhttp.readyState == 4) {
+	        if (xmlhttp.status == 200) {
+	        	response = xmlhttp.responseText;
+	        	isError=false;
+	        }
+			callback(response, isError);
+	    }
+	}
+	response=null;
+    xmlhttp.send();
+}
+
+function getQuote(dd, mm, yyyy, callback){
+	var url = "http://www.fazilettakvimi.com/"+chrome.i18n.getMessage("lang")+"/"+yyyy+"/"+mm+"/"+dd+".html";
+	invokeGetRequest(url, function(response, isError){
+		var quote = "";
+		if (!isError) {
+			var xmlDoc = $(response);
+			$("#left_top_desc span", xmlDoc).each(function(){
+				quote=$(this).text().trim();
+				console.log($(this).text().trim());
+			});
+		}
+		callback(quote, isError);
+	});
+}
+
+function updateCountries(callback){
+	var url = "http://t-services.mobilion.com.tr/DiyanetServicePub/DynNamazVakitService.asmx/GetUlkeler?filter=";
+	invokeGetRequest(url, function(response, isError){
+		var countries = [];
+		if (!isError) {
+			var xmlDoc = $.parseXML(response);
+			var map = new HashMap();
+			var country_names = [];
+			$("ArrayOfUlkeItem UlkeItem", xmlDoc).each(function(){
+				var code = $("ulkeKodu", this).text();
+				var value = $("ulkeAdi", this).text();
+				if (code==1) { // Türkiye
+					return;
+				}
+				var message = chrome.i18n.getMessage("country_"+code);
+				var name = message==""?value : message;
+				map.put(name, code);
+				country_names.push(name);
+			});
+			country_names.sort();
+
+			countries.push({code: 1, value: chrome.i18n.getMessage("country_1")}); // Türkiye
+			for (var i = 0; i < country_names.length; i++) {
+				var code=map.get(country_names[i]);
+				countries.push({code: code, value: country_names[i]});
+			};
+		}
+		callback(countries, isError);
+	});
+}
+
+function updateProvinces(a, callback){
+	var url = "http://t-services.mobilion.com.tr/DiyanetServicePub/DynNamazVakitService.asmx/GetEyaletler?filter=&ulkeKodu=" + a;
+	invokeGetRequest(url, function(response, isError){
+		var provinces = [];
+		if (!isError) {
+			var xmlDoc = $.parseXML(response);
+			$("ArrayOfEyaletItem EyaletItem", xmlDoc).each(function(){
+				var ccode = $("ulkeKodu", this).text();
+				var code = $("eyaletKodu", this).text();
+				var value = $("eyaletAdi", this).text();
+				provinces.push({code: code, value: value, countryCode: ccode});
+			});
+		}
+		callback(provinces, isError);
+	});
+}
+
+function updateCities(a, b, callback){
+	if (b == null)
+	b = "0";
+	var url = "http://t-services.mobilion.com.tr/DiyanetServicePub/DynNamazVakitService.asmx/GetSehirler?filter=&ulkeKodu=" + a + "&eyaletKod=" + b;
+	invokeGetRequest(url, function(response, isError){
+		var cities = [];
+		if (!isError) {
+			var xmlDoc = $.parseXML(response);
+			$("ArrayOfSehirItem SehirItem", xmlDoc).each(function(){
+				var ccode = $("ulkeKodu", this).text();
+				var ecode = $("eyaletKodu", this).text();
+				var evalue = $("sehirAdi", this).text();
+				var code = $("sehirId", this).text();
+				var value = $("sehirAlias", this).text();
+				if (value=="Merkez") {
+					value=chrome.i18n.getMessage("city_center");
+				}
+				var latitude = $("enlem", this).text();
+				var longitude = $("boylam", this).text();
+				cities.push({code: code, value: value, countryCode: ccode, provinceCode: ecode, provinceName: evalue, latitude:latitude, longitude:longitude });
+			});
+		}
+		callback(cities, isError);
+	});
+}
+
+function updateTimes(a, callback){
+	var url = "http://t-services.mobilion.com.tr/DiyanetServicePub/DynNamazVakitService.asmx/GetVakitler?saatFarki=0&sehirId=" + a;
+	invokeGetRequest(url, function(response, isError){
+		var times = new HashMap();
+		if (!isError) {
+			var xmlDoc = $.parseXML(response);
+			$("ArrayOfDateItem DateItem", xmlDoc).each(function(){
+				var date = $("gun", this).text();
+				var fajr = $("imsak", this).text().split(" ");
+				var sunrise = $("gunes", this).text().split(" ");
+				var zuhr = $("ogle", this).text().split(" ");
+				var asr = $("ikindi", this).text().split(" ");
+				var maghrib = $("aksam", this).text().split(" ");
+				var isha = $("yatsi", this).text().split(" ");
+				var qibla = $("kible", this).text().split(" ");
+
+				var dateDetail = date.split(".");
+
+				var yearMap = times.get(dateDetail[2]);
+				if (yearMap==null) {
+					yearMap=new HashMap();
+				}
+
+				var monthMap = yearMap.get(dateDetail[1]);
+				if (monthMap==null) {
+					monthMap=new HashMap();
+				}
+
+				var daylightSaving = new Date(dateDetail[2], (dateDetail[1]-1), dateDetail[0]);
+				var additionalHour = daylightSaving.dst()?1:0;
+
+				var dayMap = {
+					fajr   : ((parseInt(fajr[0])+additionalHour)+"").pad(2, "0")+":"+fajr[1].pad(2, "0"),
+					sunrise: ((parseInt(sunrise[0])+additionalHour)+"").pad(2, "0")+":"+sunrise[1].pad(2, "0"),
+					zuhr   : ((parseInt(zuhr[0])+additionalHour)+"").pad(2, "0")+":"+zuhr[1].pad(2, "0"),
+					asr    : ((parseInt(asr[0])+additionalHour)+"").pad(2, "0")+":"+asr[1].pad(2, "0"),
+					maghrib: ((parseInt(maghrib[0])+additionalHour)+"").pad(2, "0")+":"+maghrib[1].pad(2, "0"),
+					isha   : ((parseInt(isha[0])+additionalHour)+"").pad(2, "0")+":"+isha[1].pad(2, "0"),
+					qibla  : ((parseInt(qibla[0])+additionalHour)+"").pad(2, "0")+":"+qibla[1].pad(2, "0"),
+					quote  : null,
+					lang  : null
+				};
+
+				monthMap.put(dateDetail[0], dayMap);
+				yearMap.put(dateDetail[1], monthMap);
+				times.put(dateDetail[2], yearMap);
+			});
+		}
+		console.log(times);
+		callback(times, isError);
+	});
+}
+function getTimesOfDay(date_){
+	var date = date_.format('dd MM yyyy').split(" ");
+	var times = new HashMap(JSON.parse(localStorage["times"]));
+	var year_ = times.get(date[2]);
+	var year;
+	var month;
+	var day;
+
+	var isQuoteFound=false;
+	if (year_!=null) {
+		year = new HashMap(year_);
+		var month_ = year.get(date[1]);
+		if (month_!=null) {
+			month = new HashMap(month_);
+			day = month.get(date[0]);
+			
+			if (day!=null) {
+				return day;
+			}
+		}
+	}
+
+	return null;
+}
+
+function uuid(){
+	var s4=function () {
+	  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+	};
+	return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
+
+function timeDiff(start, end) {
+    start = start.split(":");
+    end = end.split(":");
+    var startDate = new Date(0, 0, 0, start[0], start[1], 0);
+    var endDate = new Date(0, 0, 0, end[0], end[1], 0);
+    var diff = endDate.getTime() - startDate.getTime();
+    var hours = Math.floor(diff / 1000 / 60 / 60);
+    diff -= hours * 1000 * 60 * 60;
+    var minutes = Math.floor(diff / 1000 / 60);
+
+    return {hours:hours, minutes:minutes};
+}
+function changePieSize(obj, degree){
+   degree=degree/6;
+   degree=parseInt(degree); // convert to minutes
+   degree=(degree%5)<3?(degree-degree%5):(degree+5-degree%5);
+
+   $(".num .vacC", obj).text(degree);
+
+   if (degree>30) {
+        $(".hold:eq(0)", obj).css({"-webkit-transform": "rotate(180deg)"});
+        $(".hold:eq(1) .pie", obj).css({"-webkit-transform": "rotate(180deg)"});
+        degree=degree-30;
+   } else {
+        $(".hold:eq(0)", obj).css({"-webkit-transform": "rotate(0deg)"});
+        $(".hold:eq(1) .pie", obj).css({"-webkit-transform": "rotate(0deg)"});
+   }
+   $(".hold:eq(0) .pie", obj).css({"-webkit-transform": "rotate("+(degree*6)+"deg)"});
+};
+function calcDegree(x0,y0,x1,y1){
+      var d0 = x1-x0;
+        var d1 = y1 - y0;
+         
+        var r = Math.atan2(y1, x1) - Math.atan2(y0, x0);
+        var d = (r*180)/Math.PI;
+        if (d<0) {
+             d=360+d;
+        }
+        d=d-90;
+        if (d<0) {
+             d=360+d;
+        }
+        d=360-d;
+        if (d<0) {
+             d=360+d;
+        }
+        return d;
+};
+function gmod(n,m){
+	return ((n%m)+m)%m;
+}
+function kuwaiticalendar(today, adjust){
+	if(adjust) {
+		adjustmili = 1000*60*60*24*adjust; 
+		todaymili = today.getTime()+adjustmili;
+		today = new Date(todaymili);
+	}
+	day = today.getDate();
+	month = today.getMonth();
+	year = today.getFullYear();
+	m = month+1;
+	y = year;
+	if(m<3) {
+		y -= 1;
+		m += 12;
+	}
+
+	a = Math.floor(y/100.);
+	b = 2-a+Math.floor(a/4.);
+	if(y<1583) b = 0;
+	if(y==1582) {
+		if(m>10)  b = -10;
+		if(m==10) {
+			b = 0;
+			if(day>4) b = -10;
+		}
+	}
+
+	jd = Math.floor(365.25*(y+4716))+Math.floor(30.6001*(m+1))+day+b-1524;
+
+	b = 0;
+	if(jd>2299160){
+		a = Math.floor((jd-1867216.25)/36524.25);
+		b = 1+a-Math.floor(a/4.);
+	}
+	bb = jd+b+1524;
+	cc = Math.floor((bb-122.1)/365.25);
+	dd = Math.floor(365.25*cc);
+	ee = Math.floor((bb-dd)/30.6001);
+	day =(bb-dd)-Math.floor(30.6001*ee);
+	month = ee-1;
+	if(ee>13) {
+		cc += 1;
+		month = ee-13;
+	}
+	year = cc-4716;
+
+	if(adjust) {
+		wd = gmod(jd+1-adjust,7)+1;
+	} else {
+		wd = gmod(jd+1,7)+1;
+	}
+
+	iyear = 10631./30.;
+	epochastro = 1948084;
+	epochcivil = 1948085;
+
+	shift1 = 8.01/60.;
+	
+	z = jd-epochastro;
+	cyc = Math.floor(z/10631.);
+	z = z-10631*cyc;
+	j = Math.floor((z-shift1)/iyear);
+	iy = 30*cyc+j;
+	z = z-Math.floor(j*iyear+shift1);
+	im = Math.floor((z+28.5001)/29.5);
+	if(im==13) im = 12;
+	id = z-Math.floor(29.5001*im-29);
+
+	var myRes = new Array(8);
+
+	myRes[0] = day; //calculated day (CE)
+	myRes[1] = month-1; //calculated month (CE)
+	myRes[2] = year; //calculated year (CE)
+	myRes[3] = jd-1; //julian day number
+	myRes[4] = wd-1; //weekday number
+	myRes[5] = id; //islamic date
+	myRes[6] = im; //islamic month
+	myRes[7] = iy; //islamic year
+
+	return myRes;
+}
+
+/* Definition of HashMap ++++ */
+var HashMap = function(obj) {
+  this._size = 0;
+  this._map = {};
+  if (obj) {
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        this[i] = obj[i];
+    }
+  }
+};
+
+HashMap.prototype = {
+  /**
+   * Puts the key/value pair into the map, overwriting
+   * any existing entry.
+   */
+  put: function(key, value) {
+    if (!this.containsKey(key)) {
+      this._size++;
+    }
+    this._map[key] = value;
+  },
+  
+  /**
+   * Removes the entry associated with the key
+   * and returns the removed value.
+   */
+  remove: function(key) {
+    if (this.containsKey(key)) {
+      this._size--;
+      var value = this._map[key];
+      delete this._map[key];
+      return value;
+    } else {
+      return null;
+    }
+  },
+  
+  /**
+   * Checks if this map contains the given key.
+   */
+  containsKey: function(key) {
+    return this._map.hasOwnProperty(key);
+  },
+  
+  /**
+   * Checks if this map contains the given value.
+   * Note that values are not required to be unique.
+   */
+  containsValue: function(value) {
+    for (var key in this._map) {
+      if (this._map.hasOwnProperty(key)) {
+        if (this._map[key] === value) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  },
+  
+  /**
+   * Returns the value associated with the given key.
+   */
+  get: function(key) {
+    return this.containsKey(key) ? this._map[key] : null;
+  },
+  
+  /**
+   * Clears all entries from the map.
+   */
+  clear: function() {
+    this._size = 0;
+    this._map = {};
+  },
+  
+  /**
+   * Returns an array of all keys in the map.
+   */
+  keys: function() {
+    var keys = [];
+    for (var key in this._map) {
+      if (this._map.hasOwnProperty(key)) {
+        keys.push(key);
+      }
+    }
+    return keys;
+  },
+  
+  /**
+   * Returns an array of all values in the map.
+   */
+  values: function() {
+    var values = [];
+    for (var key in this._map) {
+      if (this._map.hasOwnProperty(key)) {
+        values.push(this._map[key]);
+      }
+    }
+    return values;
+  },
+  
+  /**
+   * Returns the size of the map, which is
+   * the number of keys.
+   */
+  size: function() {
+    return this._size;
+  }
+};
+/* ---- Definition of HashMap */
+
+/* +++ Pad String +++ */
+String.prototype.pad = function (width, char) {
+  char = char || '0';
+  if (this.valueOf()==null)
+  	return null;
+
+  n = this.toString();
+  return n.length >= width ? n : new Array(width - n.length + 1).join(char) + n;
+}
+/* --- Pad String --- */
+
+/* +++ Date - format +++ */
+Date.prototype.format = function(format) {
+  var o = {
+    "M+" : this.getMonth()+1, //month
+    "d+" : this.getDate(),    //day
+    "h+" : this.getHours(),   //hour
+    "m+" : this.getMinutes(), //minute
+    "s+" : this.getSeconds(), //second
+    "q+" : Math.floor((this.getMonth()+3)/3),  //quarter
+    "S" : this.getMilliseconds() //millisecond
+  }
+
+  if(/(y+)/.test(format)) format=format.replace(RegExp.$1,
+    (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+  for(var k in o)if(new RegExp("("+ k +")").test(format))
+    format = format.replace(RegExp.$1,
+      RegExp.$1.length==1 ? o[k] :
+        ("00"+ o[k]).substr((""+ o[k]).length));
+  return format;
+}
+Date.prototype.stdTimezoneOffset = function() {
+    var jan = new Date(this.getFullYear(), 0, 1);
+    var jul = new Date(this.getFullYear(), 6, 1);
+    return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
+}
+
+Date.prototype.dst = function() {
+    return this.getTimezoneOffset() < this.stdTimezoneOffset();
+}
+/* --- Date - format --- */
